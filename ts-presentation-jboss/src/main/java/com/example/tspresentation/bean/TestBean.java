3b19bd18956e143b5aa40281a46d8f58d7a4bfa5
Ondrej Chaloupka <ochaloup@redhat.com>
2015-02-16 02:42:35 +0100

    Adding testbean as attempt

---
+++ ts-presentation-jboss/src/main/java/com/example/tspresentation/bean/TestBean.java
@@ -0,0 +1,17 @@
+package com.example.tspresentation.bean;
+
+import javax.ejb.ApplicationException;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.ejb.TransactionManagement;
+
+@Stateless
+@TransactionManagement
+public class TestBean {
+    
+    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+    public void doWork() {
+        // do something
+    }
+}
