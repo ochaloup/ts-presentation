c3972ee992dca46759d1d6a7aadf9d48460627e9
Ondra Chaloupka <ochaloup@redhat.com>
2017-02-02 10:56:18 +0100

    tuning servlet as demo for presentation to global txn handling

--- examples/ts-presentation-servlet/src/main/java/org/jboss/qa/tspresentation/servlet/MyBean.java
+++ examples/ts-presentation-servlet/src/main/java/org/jboss/qa/tspresentation/servlet/MyBean.java
@@ -10,22 +10,60 @@ import javax.jms.Queue;
 import javax.sql.DataSource;
 import javax.transaction.TransactionManager;
 
+/**
+ * <p>
+ * This bean shows multiple resources which are (or aren't) part of global (XA) transaction.<br>
+ * Please check <code>src/main/resources/standalone-full.xml</code> where definitions of particular
+ * resources are defined under activemq and datasources subsystems.
+ * <p>
+ * We have following resources defined here
+ * <ul>
+ *   <li><b>java:jboss/test-non-jta</b> : standard datasource defined with <i>jta="false"</i>,
+ *     it won't be enlisted to global transaction</li>
+ *   <li><b>java:jboss/test-jta</b> : standard datasource defined with <i>jta="true"</i>,
+ *     it will be enlisted to global transaction and will be used with LRCO</li>
+ *   <li><b>java:jboss/testxa</b> : xa datasource,
+ *     it will be enlisted to global transaction and it will participate in 2PC protocol handling</li>
+ *   <li><b>java:/ConnectionFactory</b> : connection factory,
+ *     it won't be enlisted to global transaction (the same as datasource which is non-jta)</li>
+ *   <li><b>java:/JmsNonXA</b> : pooled connection factory with <i>transaction="local"</i>,
+ *     it will be enlisted to global transaction and will be used with LRCO</li>
+ *   <li><b>java:/JmsXA</b> : pooled connection factory with <i>transaction="xa"</i>,
+ *     it will be enlisted to global transaction and it will participate in 2PC protocol handling</li>
+ * </ul>
+ *
+ * <p>
+ * <b>Disclainer:</b> transaction manager does not allow multiple non-xa resources being added to a global
+ * transaction. It's non safe and TM can't guarantee much. If you need override this behavior use system
+ * property <code>com.arjuna.ats.arjuna.allowMultipleLastResources</code> being set to <code>true</code>.
+ *
+ * <p>
+ * As result of the call of the bean we expect that all xa and jta capable resources will be rolled back
+ * and non-jta resources (connection factory and non-jta datasource) will be committed. Those are not enlisted
+ * to global transaction and are not influenced by the fact that the transaction is rolled-back.
+ * <p>
+ * This test expect to have database running where table <code>TEST</code> exits.<br>
+ * <code>CREATE TABLE TEST (id INT, a VARCHAR(255))</code>
+ */
 @Stateless
 @TransactionManagement(TransactionManagementType.CONTAINER)
 public class MyBean {
     private static final String TABLE = "test";
 
-    @Resource(lookup = "java:jboss/testxa")
-    private DataSource dsxa;
-    
+    @Resource(lookup = "java:jboss/test-non-jta")
+    private DataSource dsnonjta;
+
     @Resource(lookup = "java:jboss/test-jta")
     private DataSource dsjta;
     
-    @Resource(lookup = "java:jboss/test-non-jta")
-    private DataSource dsnonjta;
+    @Resource(lookup = "java:jboss/testxa")
+    private DataSource dsxa;
  
     @Resource(mappedName = "java:/ConnectionFactory")
     private ConnectionFactory cf;
+
+    @Resource(mappedName = "java:/JmsNonXA")
+    private ConnectionFactory nonxacf;
 
     @Resource(mappedName = "java:/JmsXA")
     private ConnectionFactory xacf;
@@ -39,21 +77,6 @@ public class MyBean {
     @Resource
     private EJBContext context;
 
-    /**
-     * <p>
-     * Expecting existence of database table TEST
-     * <p>
-     * <code>CREATE TABLE TEST (id INT, a VARCHAR(255))</code>
-     * <p>
-     * The purpose of this bean call method is shown what happens when global transaction is started
-     * and there is some non-xa resources plus some non-xa non-jta resources (datasources).<br>
-     * Transaction is marked to rollback even before 2PC starts.<br>
-     * First non-xa datasource is enlisted to transaction as LRCO resource. The second non-xa resource
-     * is neither being enlisted to transaction and is just committed as it is.
-     * <p>
-     * As result of the test we can expect having non-xa+non-jta datasource committed
-     * and non-xa connection factory message processed.
-     */
     public void call() {
     	System.out.println("Bean " + this.getClass().getName() + " was called");
 
@@ -62,6 +85,7 @@ public class MyBean {
     	DatabaseUtil.doInsert(dsnonjta, TABLE, 3, "non-xa-datasource and no jta");
 
     	JMSUtil.sendMessage(cf, queueExample, "connection factory");
+    	JMSUtil.sendMessage(nonxacf, queueExample, "non-xa connection factory");
     	JMSUtil.sendMessage(xacf, queueExample, "xa connection factory");
 
     	context.setRollbackOnly();
