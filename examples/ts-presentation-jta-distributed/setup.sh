189bae1d3e4bbf8bc99fed622144f72676dfe125
Ondra Chaloupka <ochaloup@redhat.com>
2017-11-09 14:46:56 +0100

    shell scripts fixes for jta dist example

--- examples/ts-presentation-jta-distributed/setup.sh
+++ examples/ts-presentation-jta-distributed/setup.sh
@@ -3,14 +3,14 @@
 set -ex
 
 function unzipTo {
-  local TEMP=`mktemp`
+  local TEMP=`mktemp -d`
   unzip -d "${TEMP}" "${1}"
   local DIR=`echo ${TEMP}/*`
   mv "${DIR}" "${2}"
 }
 
 WFLY_ZIP="wildfly-11.0.0.Final.zip"
-WFLY_DOWNLOAD='http://download.jboss.org/wildfly/11.0.0.Final/${WFLY_ZIP}'
+WFLY_DOWNLOAD="http://download.jboss.org/wildfly/11.0.0.Final/${WFLY_ZIP}"
 
 if [ 'x' = "x${JBOSS_HOME_1}" ] || [ 'x' = "x${JBOSS_HOME_1}" ]; then
     [ ! -f "$WFLY_ZIP" ] && wget "${WFLY_DOWNLOAD}"
@@ -27,8 +27,8 @@ if [ 'x' = "x${JBOSS_HOME_1}" ] || [ 'x' = "x${JBOSS_HOME_1}" ]; then
 fi 
 
 
-[ -d "${JBOSS_HOME_1}" ] && [ -f "${JBOSS_HOME_1}/jboss-modules.jar" ] && echo "JBOSS_HOME_1 at '${JBOSS_HOME_1}' is not a jboss directory" && exit 1
-[ -d "${JBOSS_HOME_2}" ] && [ -f "${JBOSS_HOME_2}/jboss-modules.jar" ] && echo "JBOSS_HOME_2 at '${JBOSS_HOME_2}' is not a jboss directory" && exit 1
+if [ ! -d "${JBOSS_HOME_1}" ] || [ ! -f "${JBOSS_HOME_1}/jboss-modules.jar" ]; then echo "JBOSS_HOME_1 at '${JBOSS_HOME_1}' is not a jboss directory" && exit 1; fi
+if [ ! -d "${JBOSS_HOME_2}" ] || [ ! -f "${JBOSS_HOME_2}/jboss-modules.jar" ]; then echo "JBOSS_HOME_2 at '${JBOSS_HOME_2}' is not a jboss directory" && exit 1; fi
 
 "${JBOSS_HOME_1}/bin/add-user.sh" -a -u admin123 -p Password1! --silent
 "${JBOSS_HOME_2}/bin/add-user.sh" -a -u admin123 -p Password1! --silent
@@ -39,17 +39,22 @@ cp standalone-full-server.xml "${JBOSS_HOME_2}/standalone/configuration/"
 cp postgresql*.jar "${JBOSS_HOME_1}/standalone/deployments/postgresql-driver.jar"
 cp postgresql*.jar "${JBOSS_HOME_2}/standalone/deployments/postgresql-driver.jar"
 
-cd docker
+cd dockerfile
 docker build -t postgresql-ts-presentation-jta-dist .
-docker run -p 5432:5432 -d --rm postgresql-ts-presentation-jta-dist
-docker run -p 5433:5432 -d --rm postgresql-ts-presentation-jta-dist
+docker run -p 5432:5432 -d -rm postgresql-ts-presentation-jta-dist
+docker run -p 5433:5432 -d -rm postgresql-ts-presentation-jta-dist
+cd -
 
 cd wfly-client
 mvn clean install; cp target/wfly-client.war "${JBOSS_HOME_1}/standalone/deployments/"
+cd -
 cd wfly-server
 mvn clean install; cp target/wfly-server.jar "${JBOSS_HOME_2}/standalone/deployments/"
+cd -
 
 cd "${JBOSS_HOME_1}/"
 ./bin/standalone.sh -c standalone-full-client.xml &
+cd "${JBOSS_HOME_2}/"
 ./bin/standalone.sh -c standalone-full-server.xml -Djboss.socket.binding.port-offset=100 &
+cd-
 
